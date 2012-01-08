a462de57ee045fe66cff0b97dfd40cc58a6248af
Nicolas Niemczycki <niemczycki.nicolas@gmail.com>
2012-01-08 16:46:51 +0100

    update lib setup

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
+CHIPMUNK_LIBNAME = "chipmunk"
 
 setup(
     name = "chipmunk-android",
@@ -10,5 +11,5 @@ setup(
     author = "Nicolas Niemczycki",
     author_email="niemczycki.nicolas@gmail.com",
     cmdclass = {'build_ext': build_ext},
-    ext_modules = cythonize(Extension('cymunk', ['chipmunk/chipmunk.pyx', 'chipmunk/body.pyx', 'chipmunk/shape.pyx', 'chipmunk/space.pyx']))
+    ext_modules = cythonize(Extension('chipmunk', ['chipmunk/chipmunk.pyx', 'chipmunk/body.pyx', 'chipmunk/shape.pyx', 'chipmunk/space.pyx'], libraries = [CHIPMUNK_LIBNAME]))
 )
